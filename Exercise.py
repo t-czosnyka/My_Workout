999fce188a5879ee740b3c9c81fdc054426c2dce
T.Czosnyka <t.czosnyka90@gmail.com>
2023-02-21 18:38:47 +0100

    encode_exercise function added for data export

--- Exercise.py
+++ Exercise.py
@@ -9,7 +9,11 @@ class Exercise:
         self.delay_sec = delay_sec
 
     def __str__(self):
-        return self.name + f" :worktime:{self.worktime_sec}, breaktime:{self.breaktime_sec}, rounds:{self.num_rounds}, delay:{self.delay_sec}"
+        return self.name + f" :worktime:{self.worktime_sec}, breaktime:{self.breaktime_sec}, rounds:{self.num_rounds}, " \
+                           f"delay:{self.delay_sec}"
 
 
+def encode_exercise(o: Exercise):
+    return {'name': o.name, 'worktime_sec': o.worktime_sec, 'breaktime_sec': o.breaktime_sec,
+            'num_round': o.num_rounds, 'delay_sec': o.delay_sec}
 
